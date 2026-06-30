18780fe82de9971f4f04dc8bb2b34f8231ff1b9f
nianyi-hub <945585924@qq.com>
2026-06-30 15:21:43 +0800

    备份测试项目代码

---
+++ ISO11820System.Tests/LoginTests.cs
@@ -0,0 +1,11 @@
+namespace ISO11820System.Tests
+{
+    public class LoginTests
+    {
+        [Fact]
+        public void Login_ValidUser_ReturnsTrue()
+        {
+            // TODO: 实现登录验证测试
+        }
+    }
+}
