ff608cbd2b449585bfb0630ef47d0fdb2e025396
huarh00 <huarh00@gmail.com>
2026-07-04 13:03:12 +0800

    Add data annotations for display names in Operator class

--- src/Models/Operator.cs
+++ src/Models/Operator.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,15 +13,18 @@ namespace ISO11820System.Models
     public class Operator
     {
         /// <summary>用户ID</summary>
+        [Display(Name = "用户ID")]
         public string UserId { get; set; } = string.Empty;
 
         /// <summary>用户名（登录用）</summary>
+        [Display(Name = "用户名")]
         public string Username { get; set; } = string.Empty;
 
         /// <summary>密码（明文存储）</summary>
         public string Password { get; set; } = string.Empty;
 
         /// <summary>用户类型：admin=管理员, operator=试验员</summary>
+        [Display(Name = "用户类型")]
         public string UserType { get; set; } = string.Empty;
     }
 }
